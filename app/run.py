f757f4aa171fa80829f413ba21f66c85c5516876
Uzair <uzairaslam92@gmail.com>
2020-04-29 13:14:59 +0200

    Add files via upload

---
+++ app/run.py
@@ -0,0 +1,152 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar, Pie
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('df', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    #sum of weather related meassages
+    category_weather_related = df[['weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
+                                   'other_weather']].sum().sort_values(ascending=False)
+    category_weather_names = list(category_weather_related.index)
+    
+    #sum of requests and offers related messages
+    request_offer_sum = df[['request', 'offer']].sum().sort_values(ascending=False)
+    request_offer_names = list(request_offer_sum.index)
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=category_weather_names,
+                    y=category_weather_related
+                )
+            ],
+
+            'layout': {
+                'title': 'Sum of Weather Related Messages',
+                'titlefont' : {
+                        'color':'grey',
+                        'size':'18',
+                    },
+                'yaxis': {
+                    'title': "Total sum"
+                },
+                'xaxis': {
+                    'title': "Different weather related disasters"
+                }
+            }
+        },
+        {
+            'data': [
+                Pie(
+                    labels=request_offer_names,
+                    values=request_offer_sum,
+                    showlegend=False,
+                    textinfo="label+percent",
+                    textposition='outside'
+                )
+                
+            ],
+
+            'layout': {
+                'titlefont' : {
+                        'color':'grey',
+                        'size':'18',
+                    },
+                'title': 'Total Offers and Requests'
+          
+            }
+        }
+        
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
